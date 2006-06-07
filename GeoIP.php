dcf165fbb14ae5bae3c470394de60b51f4cc46f8
Hans Lellelid <hlellelid@php.net>
2006-06-07 16:34:25 +0000

    Removed some line breaks in country names.
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_GeoIP/trunk@214400 c90b9560-bf6c-de11-be94-00142212c4b1

--- GeoIP.php
+++ GeoIP.php
@@ -168,58 +168,58 @@ class Net_GeoIP
         "UKR","UGA","UM","USA","URY","UZB","VAT","VCT","VEN","VGB","VIR","VNM","VUT",
         "WLF","WSM","YEM","YT","YUG","ZAF","ZMB","ZR","ZWE","A1","A2","O1");
 
-    public static $COUNTRY_NAMES = array(
-        "", "Asia/Pacific Region", "Europe", "Andorra", "United Arab Emirates",
-        "Afghanistan", "Antigua and Barbuda", "Anguilla", "Albania", "Armenia",
-        "Netherlands Antilles", "Angola", "Antarctica", "Argentina", "American Samoa",
-        "Austria", "Australia", "Aruba", "Azerbaijan", "Bosnia and Herzegovina",
-        "Barbados", "Bangladesh", "Belgium", "Burkina Faso", "Bulgaria", "Bahrain",
-        "Burundi", "Benin", "Bermuda", "Brunei Darussalam", "Bolivia", "Brazil",
-        "Bahamas", "Bhutan", "Bouvet Island", "Botswana", "Belarus", "Belize",
-        "Canada", "Cocos (Keeling) Islands", "Congo, The Democratic Republic of the",
-        "Central African Republic", "Congo", "Switzerland", "Cote D'Ivoire", "Cook
-        Islands", "Chile", "Cameroon", "China", "Colombia", "Costa Rica", "Cuba", "Cape
-        Verde", "Christmas Island", "Cyprus", "Czech Republic", "Germany", "Djibouti",
-        "Denmark", "Dominica", "Dominican Republic", "Algeria", "Ecuador", "Estonia",
-        "Egypt", "Western Sahara", "Eritrea", "Spain", "Ethiopia", "Finland", "Fiji",
-        "Falkland Islands (Malvinas)", "Micronesia, Federated States of", "Faroe
-        Islands", "France", "France, Metropolitan", "Gabon", "United Kingdom",
-        "Grenada", "Georgia", "French Guiana", "Ghana", "Gibraltar", "Greenland",
-        "Gambia", "Guinea", "Guadeloupe", "Equatorial Guinea", "Greece", "South Georgia
-        and the South Sandwich Islands", "Guatemala", "Guam", "Guinea-Bissau",
-        "Guyana", "Hong Kong", "Heard Island and McDonald Islands", "Honduras",
-        "Croatia", "Haiti", "Hungary", "Indonesia", "Ireland", "Israel", "India",
-        "British Indian Ocean Territory", "Iraq", "Iran, Islamic Republic of",
-        "Iceland", "Italy", "Jamaica", "Jordan", "Japan", "Kenya", "Kyrgyzstan",
-        "Cambodia", "Kiribati", "Comoros", "Saint Kitts and Nevis", "Korea, Democratic
-        People's Republic of", "Korea, Republic of", "Kuwait", "Cayman Islands",
-        "Kazakstan", "Lao People's Democratic Republic", "Lebanon", "Saint Lucia",
-        "Liechtenstein", "Sri Lanka", "Liberia", "Lesotho", "Lithuania", "Luxembourg",
-        "Latvia", "Libyan Arab Jamahiriya", "Morocco", "Monaco", "Moldova, Republic
-        of", "Madagascar", "Marshall Islands", "Macedonia", "Mali", "Myanmar", "Mongolia", 
-        "Macau", "Northern Mariana Islands",
-        "Martinique", "Mauritania", "Montserrat", "Malta", "Mauritius", "Maldives",
-        "Malawi", "Mexico", "Malaysia", "Mozambique", "Namibia", "New Caledonia",
-        "Niger", "Norfolk Island", "Nigeria", "Nicaragua", "Netherlands", "Norway",
-        "Nepal", "Nauru", "Niue", "New Zealand", "Oman", "Panama", "Peru", "French
-        Polynesia", "Papua New Guinea", "Philippines", "Pakistan", "Poland", "Saint
-        Pierre and Miquelon", "Pitcairn Islands", "Puerto Rico", "Palestinian Territory,
-        Occupied", "Portugal", "Palau", "Paraguay", "Qatar", "Reunion", "Romania",
-        "Russian Federation", "Rwanda", "Saudi Arabia", "Solomon Islands",
-        "Seychelles", "Sudan", "Sweden", "Singapore", "Saint Helena", "Slovenia",
-        "Svalbard and Jan Mayen", "Slovakia", "Sierra Leone", "San Marino", "Senegal",
-        "Somalia", "Suriname", "Sao Tome and Principe", "El Salvador", "Syrian Arab
-        Republic", "Swaziland", "Turks and Caicos Islands", "Chad", "French Southern
-        Territories", "Togo", "Thailand", "Tajikistan", "Tokelau", "Turkmenistan",
-        "Tunisia", "Tonga", "East Timor", "Turkey", "Trinidad and Tobago", "Tuvalu",
-        "Taiwan", "Tanzania, United Republic of", "Ukraine",
-        "Uganda", "United States Minor Outlying Islands", "United States", "Uruguay",
-        "Uzbekistan", "Holy See (Vatican City State)", "Saint Vincent and the
-        Grenadines", "Venezuela", "Virgin Islands, British", "Virgin Islands, U.S.",
-        "Vietnam", "Vanuatu", "Wallis and Futuna", "Samoa", "Yemen", "Mayotte",
-        "Yugoslavia", "South Africa", "Zambia", "Zaire", "Zimbabwe",
-        "Anonymous Proxy","Satellite Provider","Other"
-        );
+	public static $COUNTRY_NAMES = array(
+		"", "Asia/Pacific Region", "Europe", "Andorra", "United Arab Emirates",
+		"Afghanistan", "Antigua and Barbuda", "Anguilla", "Albania", "Armenia",
+		"Netherlands Antilles", "Angola", "Antarctica", "Argentina", "American Samoa",
+		"Austria", "Australia", "Aruba", "Azerbaijan", "Bosnia and Herzegovina",
+		"Barbados", "Bangladesh", "Belgium", "Burkina Faso", "Bulgaria", "Bahrain",
+		"Burundi", "Benin", "Bermuda", "Brunei Darussalam", "Bolivia", "Brazil",
+		"Bahamas", "Bhutan", "Bouvet Island", "Botswana", "Belarus", "Belize",
+		"Canada", "Cocos (Keeling) Islands", "Congo, The Democratic Republic of the",
+		"Central African Republic", "Congo", "Switzerland", "Cote D'Ivoire", "Cook Islands", 
+		"Chile", "Cameroon", "China", "Colombia", "Costa Rica", "Cuba", "Cape Verde", 
+		"Christmas Island", "Cyprus", "Czech Republic", "Germany", "Djibouti",
+		"Denmark", "Dominica", "Dominican Republic", "Algeria", "Ecuador", "Estonia",
+		"Egypt", "Western Sahara", "Eritrea", "Spain", "Ethiopia", "Finland", "Fiji",
+		"Falkland Islands (Malvinas)", "Micronesia, Federated States of", "Faroe Islands", 
+		"France", "France, Metropolitan", "Gabon", "United Kingdom",
+		"Grenada", "Georgia", "French Guiana", "Ghana", "Gibraltar", "Greenland",
+		"Gambia", "Guinea", "Guadeloupe", "Equatorial Guinea", "Greece", 
+		"South Georgia and the South Sandwich Islands", "Guatemala", "Guam", "Guinea-Bissau",
+		"Guyana", "Hong Kong", "Heard Island and McDonald Islands", "Honduras",
+		"Croatia", "Haiti", "Hungary", "Indonesia", "Ireland", "Israel", "India",
+		"British Indian Ocean Territory", "Iraq", "Iran, Islamic Republic of",
+		"Iceland", "Italy", "Jamaica", "Jordan", "Japan", "Kenya", "Kyrgyzstan",
+		"Cambodia", "Kiribati", "Comoros", "Saint Kitts and Nevis", 
+		"Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Cayman Islands",
+		"Kazakstan", "Lao People's Democratic Republic", "Lebanon", "Saint Lucia",
+		"Liechtenstein", "Sri Lanka", "Liberia", "Lesotho", "Lithuania", "Luxembourg",
+		"Latvia", "Libyan Arab Jamahiriya", "Morocco", "Monaco", "Moldova, Republic of", 
+		"Madagascar", "Marshall Islands", "Macedonia", "Mali", "Myanmar", "Mongolia", 
+		"Macau", "Northern Mariana Islands",
+		"Martinique", "Mauritania", "Montserrat", "Malta", "Mauritius", "Maldives",
+		"Malawi", "Mexico", "Malaysia", "Mozambique", "Namibia", "New Caledonia",
+		"Niger", "Norfolk Island", "Nigeria", "Nicaragua", "Netherlands", "Norway",
+		"Nepal", "Nauru", "Niue", "New Zealand", "Oman", "Panama", "Peru", "French Polynesia", 
+		"Papua New Guinea", "Philippines", "Pakistan", "Poland", "Saint Pierre and Miquelon", 
+		"Pitcairn Islands", "Puerto Rico", "Palestinian Territory, Occupied", 
+		"Portugal", "Palau", "Paraguay", "Qatar", "Reunion", "Romania",
+		"Russian Federation", "Rwanda", "Saudi Arabia", "Solomon Islands",
+		"Seychelles", "Sudan", "Sweden", "Singapore", "Saint Helena", "Slovenia",
+		"Svalbard and Jan Mayen", "Slovakia", "Sierra Leone", "San Marino", "Senegal",
+		"Somalia", "Suriname", "Sao Tome and Principe", "El Salvador", "Syrian Arab Republic", "Swaziland", 
+		"Turks and Caicos Islands", "Chad", "French Southern Territories", 
+		"Togo", "Thailand", "Tajikistan", "Tokelau", "Turkmenistan",
+		"Tunisia", "Tonga", "East Timor", "Turkey", "Trinidad and Tobago", "Tuvalu",
+		"Taiwan", "Tanzania, United Republic of", "Ukraine",
+		"Uganda", "United States Minor Outlying Islands", "United States", "Uruguay",
+		"Uzbekistan", "Holy See (Vatican City State)", "Saint Vincent and the Grenadines", 
+		"Venezuela", "Virgin Islands, British", "Virgin Islands, U.S.",
+		"Vietnam", "Vanuatu", "Wallis and Futuna", "Samoa", "Yemen", "Mayotte",
+		"Yugoslavia", "South Africa", "Zambia", "Zaire", "Zimbabwe",
+		"Anonymous Proxy","Satellite Provider","Other"
+		);
             
     // storage / caching flags
     const STANDARD = 0;
