4674a4a7645619fcd4ed20872220fe6cd90b121d
Daniel O'Connor <clockwerx@php.net>
2010-03-25 00:42:38 +0000

    Bug #17262 	Net_GeoIP_Location setter not valid - test coverage
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_GeoIP/trunk@296762 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Net_GeoIPTest.php
+++ tests/Net_GeoIPTest.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'PHPUnit/Framework/TestCase.php';
+require_once 'Net/GeoIP.php';
 
 class Net_GeoIPTest extends PHPUnit_Framework_TestCase
 {
@@ -23,5 +24,32 @@ class Net_GeoIPTest extends PHPUnit_Framework_TestCase
     public function lookupRegion($addr)
     public function open($filename, $flags = null)
     public static function getInstance($filename = null, $flags = null)');
+    }
+
+    public function testBug17262() {
+        $path = dirname(dirname(__FILE__)) . '/data/GeoLiteCity.dat';
+
+        if (!file_exists($path)) {
+            $this->markTestSkipped("Could not find GeoLiteCity.dat in " . $path . "\nObtain from http://www.maxmind.com/app/geolitecity");
+        }
+
+        $geoip = Net_GeoIP::getInstance($path);
+        $location = $geoip->lookupLocation('24.24.24.24');
+
+        $location = new Net_GeoIP_Location();
+        $location->set('countryCode', 'US');
+
+        $this->assertSame(array('countryCode' => 'US',
+                                'countryCode3' => 'USA',
+                                'countryName' => 'United States',
+                                'region' => 'NY',
+                                'city' => 'Jamaica',
+                                'postalCode' => '11434',
+                                'latitude' => 40.6763,
+                                'longitude' => -73.7752,
+                                'areaCode' => 718,
+                                'dmaCode' => 501
+                                ),
+                        $location->getData());
     }
 }
